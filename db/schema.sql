801a2f270cbc134a76770a860578cc9ffdb72c25
Adam Bilsky <adambilsky@gmail.com>
2018-09-01 16:05:10 -0500

    schema and connection files

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+-- Main database file
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(40) NOT NULL,
+    devoured BOOLEAN DEFAULT false,
+    PRIMARY KEY (id)
+
+)
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Kopp's Doublecheese With Everything", 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Spears PBJ Bacon Cheese", 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Solly's Butterburger", 0);
