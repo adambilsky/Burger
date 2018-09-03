6b4ea08b82895a784cdd7a5fef1a0787cf88add3
Adam Bilsky <adambilsky@gmail.com>
2018-09-03 12:28:22 -0500

    Monday push ORM update

--- config/orm.js
+++ config/orm.js
@@ -6,39 +6,59 @@ var connection = require("./connection.js");
 
 // The ?? signs are for swapping out table or column names
 // The ? signs are for swapping out other values
-// These help avoid SQL injection
-// https://en.wikipedia.org/wiki/SQL_injection
+var tableName = "burgers_db";
+
 var orm = {
-    selectAll: function (tableInput) {
-        var queryString = "SELECT * FROM ?";
-        connection.query(queryString, tableInput, function (err, result) {
-            if (err) throw err;
-            console.log(result);
+    // *** NOTE: IN ALL FOLLOWING, the variable 'querystring' is replaced by 's'
+
+    // Here our ORM is creating a simple method for performing a query of the entire table.
+    // We make use of the callback to ensure that data is returned only once the query is done.
+    selectAll: function (callback) {
+        var s = "SELECT * FROM " + tableName;
+
+        connection.query(s, function (err, result) {
+
+            callback(result);
+
         });
     },
-    insertOne: function (whatToSelect, table, //orderCol
-        ){
-        var queryString = //"SELECT ?? FROM ?? ORDER BY ?? DESC";
-            console.log(queryString);
-        connection.query(queryString, //[whatToSelect, table, orderCol], 
-            function (err, result) {
-                if (err) throw err;
-                console.log(result);
-            });
-    },
-    updateOne: function (//tableOneCol, tableTwoForeignKey, tableOne, tableTwo
-            ) {
-        var queryString =
-            //"SELECT ??, COUNT(??) AS count FROM ?? LEFT JOIN ?? ON ??.??= ??.id GROUP BY ?? ORDER BY count DESC LIMIT 1";
 
-        connection.query(
-            queryString,
-            //[tableOneCol, tableOneCol, tableOne, tableTwo, tableTwo, tableTwoForeignKey, tableOne, tableOneCol],
-            function (err, result) {
-                if (err) throw err;
-                console.log(result);
-            }
-        );
+    // *****************************
+    // COMPARE THE FOLLOWING TO THE addToDo CODE BELOW
+    // insertOne: function (whatToSelect, table, orderCol) {
+    //     var queryString = //"SELECT ?? FROM ?? ORDER BY ?? DESC";
+    //         console.log(queryString);
+    //     connection.query(queryString, //[whatToSelect, table, orderCol], 
+    //         function (err, result) {
+    //             if (err) throw err;
+    //             console.log(result);
+    //         });
+    // },
+
+    // *****************************
+    // INSERT/MODIFY ORM-SEQUELIZE CODE HERE AND MODIFY
+    insertOne: function (newBurger, callback) {
+        var s = "INSERT INTO " + tableName + " (text, devoured) VALUES (?,?)";
+        newBurger.devoured = newBurger.devoured || 0;
+        connection.query(s, [
+            newBurger.text, newBurger.devoured
+        ], function (err, result) {
+
+            callback(result);
+
+        });
+    },
+
+    updateOne: function (burger, callback) {
+        var s = "UPDATE " + tableName + " SET text=? WHERE id=?";
+
+        connection.query(s, [
+            burger.text, burger.id
+        ], function (err, result) {
+
+            callback(result);
+
+        });
     }
 };
 
