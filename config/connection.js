795829f227d154f8ddf02aec16ddefa64c6e4f9f
Adam Bilsky <adambilsky@gmail.com>
2018-09-09 19:05:53 -0500

    Heroku fix

--- config/connection.js
+++ config/connection.js
@@ -8,16 +8,16 @@ if (process.env.JAWSDB_URL) {
 }
 else {
   connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "burgers_db"
-});
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "burgers_db"
+  });
 };
 
 // Make the connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,5 @@
 -- Main database file
-DROP DATABASE IF EXISTS burgers_db;
-CREATE DATABASE burgers_db;
+
 USE burgers_db;
 
 CREATE TABLE burgers
